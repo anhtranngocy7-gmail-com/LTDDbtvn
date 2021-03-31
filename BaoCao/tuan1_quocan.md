3163da87eb3256fabc71ec578425dd476e4fbe54
NguyenQuocAn99 <1720024@student.hcmus.edu.vn>
2021-03-31 14:20:04 +0700

    thêm lại activity bị xóa

--- BaoCao/tuan1_quocan.md
+++ BaoCao/tuan1_quocan.md
@@ -1,2 +1,2 @@
-
+#Complete
 ![login](../HinhAnh/nhom3_login_an.png)
--- Tuan1/app/src/main/java/com/laptrinhdidong/nhom3/btvntuan1/MainActivity.kt
+++ Tuan1/app/src/main/java/com/laptrinhdidong/nhom3/btvntuan1/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.nhom3_login_an)
+        setContentView(R.layout.activity_main)
     }
 }
