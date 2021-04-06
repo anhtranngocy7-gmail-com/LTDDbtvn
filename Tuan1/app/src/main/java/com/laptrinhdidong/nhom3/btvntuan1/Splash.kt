46695804a07ae2b1e9d5358eb54e8162133873fa
Thanh Binh Le <binh.lethanh1907@gmail.com>
2021-04-06 16:03:14 +0700

    binh_onboarding2

---
+++ Tuan1/app/src/main/java/com/laptrinhdidong/nhom3/btvntuan1/Splash.kt
@@ -0,0 +1,11 @@
+package com.laptrinhdidong.nhom3.btvntuan1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.nhom3_binh_splash)
+    }
+}
