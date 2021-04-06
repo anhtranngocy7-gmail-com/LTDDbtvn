95fc59d4b9968aa8e380f4eb5005d74533b68e6b
nvl99 <nqan1999vvk@gmail.com>
2021-04-06 23:08:14 +0700

    sữa lối gradle _an

--- Tuan1/app/src/main/java/com/laptrinhdidong/nhom3/btvntuan1/LoginActivity.kt
+++ Tuan1/app/src/main/java/com/laptrinhdidong/nhom3/btvntuan1/LoginActivity.kt
@@ -2,12 +2,16 @@ package com.laptrinhdidong.nhom3.btvntuan1
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.nhom3_login_an.*
+import android.widget.Button
+import android.widget.EditText
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.nhom3_login_an)
+        var btn_login=findViewById<Button>(R.id.btn_login);
+        var et_email_login=findViewById<EditText>(R.id.et_email_login);
+        var et_pass_login=findViewById<EditText>(R.id.et_pass_login);
         btn_login.setOnClickListener{
             if(et_email_login.text.toString()=="ronaldo@gmail.com"&&et_pass_login.text.toString()=="123456")
             {
