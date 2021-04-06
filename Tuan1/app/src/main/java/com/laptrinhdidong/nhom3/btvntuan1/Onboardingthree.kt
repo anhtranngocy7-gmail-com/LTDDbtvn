9ad5a02981d4f16ee0403a9d56038d4391cf5e2e
Nguyen Hoang Quoc <nhquoc99@gmail.com>
2021-04-06 16:07:47 +0700

    btvn tuan 2 quoc

---
+++ Tuan1/app/src/main/java/com/laptrinhdidong/nhom3/btvntuan1/Onboardingthree.kt
@@ -0,0 +1,12 @@
+package com.laptrinhdidong.nhom3.btvntuan1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Onboardingthree : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.nhom3_quoc_onboarding3)
+    }
+}
