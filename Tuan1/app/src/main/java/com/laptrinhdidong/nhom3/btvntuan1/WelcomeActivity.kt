f0480bb80d5ef14e224210a10931cb00a99165b0
Dat Nguyen <ngtiendat568@gmail.com>
2021-04-07 20:31:19 +0700

    HW2_FLOWAPP_SPLASH_ONBOARDING_WELCOME_LOGIN_PROFILE

--- Tuan1/app/src/main/java/com/laptrinhdidong/nhom3/btvntuan1/WelcomeActivity.kt
+++ Tuan1/app/src/main/java/com/laptrinhdidong/nhom3/btvntuan1/WelcomeActivity.kt
@@ -1,13 +1,22 @@
 package com.laptrinhdidong.nhom3.btvntuan1
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.Button
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.nhom3_binh_welcome)
+        val btn_signin = findViewById<Button>(R.id.signin)
+
+        btn_signin.setOnClickListener {
+            val intent = Intent(this@WelcomeActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 
     override fun onStart() {
