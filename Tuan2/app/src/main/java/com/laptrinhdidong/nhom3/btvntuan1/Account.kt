dd7b162f718c87dd31e46d2d33eda89016371aa4
nvl99 <nqan1999vvk@gmail.com>
2021-04-13 21:28:44 +0700

    phần đầu bài tập tuần 3

---
+++ Tuan2/app/src/main/java/com/laptrinhdidong/nhom3/btvntuan1/Account.kt
@@ -0,0 +1,5 @@
+package com.laptrinhdidong.nhom3.btvntuan1
+
+class Account(var fullname:String, var email:String,var password:String) {
+
+}
--- Tuan2/app/src/main/java/com/laptrinhdidong/nhom3/btvntuan1/ProfileActivity.kt
+++ Tuan2/app/src/main/java/com/laptrinhdidong/nhom3/btvntuan1/ProfileActivity.kt
@@ -1,8 +1,8 @@
 package com.laptrinhdidong.nhom3.btvntuan1
 
 import android.os.Bundle
-import android.support.v7.app.AlertDialog
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
