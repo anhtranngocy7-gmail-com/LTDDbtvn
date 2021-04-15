45264b6c5ea6ed303a1b5bbbedac3abddc5d3f99
nvl99 <nqan1999vvk@gmail.com>
2021-04-15 18:40:13 +0700

    them livedata cho profile

--- Tuan2/app/src/main/java/com/laptrinhdidong/nhom3/btvntuan1/LoginViewModel.kt
+++ Tuan2/app/src/main/java/com/laptrinhdidong/nhom3/btvntuan1/LoginViewModel.kt
@@ -3,6 +3,7 @@ package com.laptrinhdidong.nhom3.btvntuan1
 import androidx.lifecycle.ViewModel
 
 class LoginViewModel : ViewModel(){
-    var account : Account = Account("default fullname", "", "default password","")
+
+    var account : Account = Account("default fullname", "", "","+84")
 
 }
