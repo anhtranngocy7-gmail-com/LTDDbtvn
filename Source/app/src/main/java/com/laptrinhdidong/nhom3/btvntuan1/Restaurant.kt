6bd818c7b670b30573323e26f19006b85bbef220
Dat Nguyen <ngtiendat568@gmail.com>
2021-04-19 22:24:29 +0700

    Create Restaurant object + getDataSet() 600 objects

---
+++ Source/app/src/main/java/com/laptrinhdidong/nhom3/btvntuan1/Restaurant.kt
@@ -0,0 +1,5 @@
+package com.laptrinhdidong.nhom3.btvntuan1
+
+class Restaurant(var ID: String, var Name: String, var Address: String, var District: String, var City: String, var AvatarURL: String) {
+
+}
