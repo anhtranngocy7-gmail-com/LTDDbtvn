6eb15d712dfc2ac78bd14cba8f129a37c474d885
Thanh Binh Le <binh.lethanh1907@gmail.com>
2021-04-20 01:07:59 +0700

    Validate SignUp Account

--- Source/app/src/main/java/com/laptrinhdidong/nhom3/btvntuan1/Activity/SignUp/SignUpActivity.kt
+++ Source/app/src/main/java/com/laptrinhdidong/nhom3/btvntuan1/Activity/SignUp/SignUpActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.databinding.DataBindingUtil
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
@@ -18,13 +19,61 @@ class SignUpActivity : AppCompatActivity()  {
         viewModel=ViewModelProvider(this).get(SignUpViewModel::class.java)
         binding=DataBindingUtil.setContentView(this,R.layout.nhom3_quoc_signup)
         binding.btnSignup123.setOnClickListener {
-            viewModel.account.fullname=binding.editTextFullname.text.toString().trim()
-            viewModel.account.email=binding.editTextEmail.text.toString().trim()
-            viewModel.account.password=binding.editTextPassword.text.toString().trim()
-            var accountTempt : Account = Account(viewModel.account.fullname, viewModel.account.email, viewModel.account.password,"+84")
-            DataStore.addAccount(accountTempt)
-            val intent = Intent(this@SignUpActivity,LoginActivity::class.java)
-            startActivity(intent)
+            if (registerUser(binding.btnSignup123)) {
+                viewModel.account.fullname = binding.editTextFullname.text.toString().trim()
+                viewModel.account.email = binding.editTextEmail.text.toString().trim()
+                viewModel.account.password = binding.editTextPassword.text.toString().trim()
+                var accountTempt: Account = Account(
+                    viewModel.account.fullname,
+                    viewModel.account.email,
+                    viewModel.account.password,
+                    "+84"
+                )
+
+                DataStore.addAccount(accountTempt)
+
+                val intent = Intent(this@SignUpActivity, LoginActivity::class.java)
+                startActivity(intent)
+            }
         }
+    }
+    fun validateEmail(): Boolean{
+        var etEmail = binding.editTextEmail.text.toString()
+        var emailPattern = "[a-zA-z0._-]+@[a-z]+\\.+[a-z]+"
+        if (etEmail.isEmpty()){
+            binding.editTextEmail.setError("Field cannot be empty")
+            return false
+        }
+        else if (!etEmail.matches(Regex(emailPattern))){
+            binding.editTextEmail.setError("Invalid email address")
+            return false
+        }
+        else{
+            binding.editTextEmail.setError(null)
+            return true
+        }
+    }
+    fun validatePassword(): Boolean{
+        var etPassword = binding.editTextPassword.text.toString()
+        var passwordVal = "(?=.*[0-9])" + "(?=.*[a-z])" + "(?=.*[A-Z])" + "(?=.*[!@#$%^&*()])" + ".{8,}"
+        if (etPassword.isEmpty()){
+            binding.editTextPassword.setError("Field cannot be empty")
+            return false
+        }
+        else if (!etPassword.matches(Regex(passwordVal))){
+            binding.editTextPassword.setError("Password is too weak")
+            return false
+        }
+        else{
+            binding.editTextPassword.setError(null)
+            return true
+        }
+    }
+    fun registerUser(view: View): Boolean {
+        if (!validatePassword() or !validateEmail()) {
+            return false;
+        }
+        else
+            return true
     }
 }
