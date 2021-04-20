2a8a7404cffce2e9de65db6b39082f871b5abfad
Anh Tran <anhtranngocy7@gmail.com>
2021-04-20 23:11:44 +0700

    hoan thanh sign up

--- Source/app/src/main/java/com/laptrinhdidong/nhom3/btvntuan1/Activity/SignUp/SignUpActivity.kt
+++ Source/app/src/main/java/com/laptrinhdidong/nhom3/btvntuan1/Activity/SignUp/SignUpActivity.kt
@@ -13,13 +13,13 @@ import com.laptrinhdidong.nhom3.btvntuan1.databinding.Nhom3QuocSignupBinding
 class SignUpActivity : AppCompatActivity()  {
     private lateinit var binding: Nhom3QuocSignupBinding
     private lateinit var viewModel: SignUpViewModel
-    private var account : Account = Account("Default fullname", "Default email", "Default password","Default phone")
+    private var account : Account = Account("", "", "","")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel=ViewModelProvider(this).get(SignUpViewModel::class.java)
         binding=DataBindingUtil.setContentView(this,R.layout.nhom3_quoc_signup)
         binding.btnSignup123.setOnClickListener {
-            if (registerUser(binding.btnSignup123)) {
+            if (viewModel.registerUser(binding.btnSignup123,binding)) {
                 viewModel.account.fullname = binding.editTextFullname.text.toString().trim()
                 viewModel.account.email = binding.editTextEmail.text.toString().trim()
                 viewModel.account.password = binding.editTextPassword.text.toString().trim()
@@ -29,51 +29,11 @@ class SignUpActivity : AppCompatActivity()  {
                     viewModel.account.password,
                     "+84"
                 )
-
                 DataStore.addAccount(accountTempt)
-
                 val intent = Intent(this@SignUpActivity, LoginActivity::class.java)
                 startActivity(intent)
             }
         }
     }
-    fun validateEmail(): Boolean{
-        var etEmail = binding.editTextEmail.text.toString()
-        var emailPattern = "[a-zA-z0._-]+@[a-z]+\\.+[a-z]+"
-        if (etEmail.isEmpty()){
-            binding.editTextEmail.setError("Field cannot be empty")
-            return false
-        }
-        else if (!etEmail.matches(Regex(emailPattern))){
-            binding.editTextEmail.setError("Invalid email address")
-            return false
-        }
-        else{
-            binding.editTextEmail.setError(null)
-            return true
-        }
-    }
-    fun validatePassword(): Boolean{
-        var etPassword = binding.editTextPassword.text.toString()
-        var passwordVal = "(?=.*[0-9])" + "(?=.*[a-z])" + "(?=.*[A-Z])" + "(?=.*[!@#$%^&*()])" + ".{8,}"
-        if (etPassword.isEmpty()){
-            binding.editTextPassword.setError("Field cannot be empty")
-            return false
-        }
-        else if (!etPassword.matches(Regex(passwordVal))){
-            binding.editTextPassword.setError("Password is too weak")
-            return false
-        }
-        else{
-            binding.editTextPassword.setError(null)
-            return true
-        }
-    }
-    fun registerUser(view: View): Boolean {
-        if (!validatePassword() or !validateEmail()) {
-            return false;
-        }
-        else
-            return true
-    }
+
 }
--- Source/app/src/main/java/com/laptrinhdidong/nhom3/btvntuan1/Activity/SignUp/SignUpViewModel.kt
+++ Source/app/src/main/java/com/laptrinhdidong/nhom3/btvntuan1/Activity/SignUp/SignUpViewModel.kt
@@ -1,9 +1,48 @@
 package com.laptrinhdidong.nhom3.btvntuan1
 
+import android.view.View
+import android.widget.EditText
 import androidx.lifecycle.ViewModel
+import com.laptrinhdidong.nhom3.btvntuan1.databinding.Nhom3QuocSignupBinding
 
 
 class SignUpViewModel : ViewModel()
 {
-    var account : Account=Account("default","default","default","+84")
+    var account : Account=Account("","","","")
+    fun validateEmail(binding: Nhom3QuocSignupBinding): Boolean{
+        var etEmail = binding.editTextEmail.text.toString()
+        var emailPattern = "[a-zA-z0._-]+@[a-z]+\\.+[a-z]+"
+        if (etEmail.isEmpty()){
+            binding.editTextEmail.error = "Field cannot be empty"
+            return false
+        }
+        else if (!etEmail.matches(Regex(emailPattern))){
+            binding.editTextEmail.error = "Invalid email address"
+            return false
+        }
+        else {
+            binding.editTextEmail.error = null
+            return true
+        }
+    }
+    fun validatePassword(binding: Nhom3QuocSignupBinding): Boolean{
+        var etPassword = binding.editTextPassword.text.toString()
+        var passwordVal = "(?=.*[0-9])" + "(?=.*[a-z])" + "(?=.*[A-Z])" + "(?=.*[!@#$%^&*()])" + ".{8,}"
+        if (etPassword.isEmpty()){
+            binding.editTextPassword.error = "Field cannot be empty"
+            return false
+        }
+        else if (!etPassword.matches(Regex(passwordVal))){
+            binding.editTextPassword.error = "Password is too weak"
+            return false
+        }
+        else{
+            binding.editTextPassword.error = null
+            return true
+        }
+    }
+    fun registerUser(view: View,binding: Nhom3QuocSignupBinding): Boolean {
+        return !(!validatePassword(binding) or !validateEmail(binding))
+    }
+
 }
