d5f127fc81ebcc5276a209d25bfc6f8086f84b47
NguyenQuocAn99 <nqan1999vvk@gmail.com>
2021-04-28 21:34:06 +0700

    An_Sửa bài W4, call data

--- Source/app/src/main/java/com/laptrinhdidong/nhom3/btvntuan1/Activity/ListRestaurant/ListRestaurantViewModel.kt
+++ Source/app/src/main/java/com/laptrinhdidong/nhom3/btvntuan1/Activity/ListRestaurant/ListRestaurantViewModel.kt
@@ -6,7 +6,9 @@ import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.laptrinhdidong.nhom3.btvntuan1.R
+import com.laptrinhdidong.nhom3.btvntuan1.DataStore.getDataSet
 
 class ListRestaurantViewModel() : ViewModel() {
     var layoutManager : Boolean =false
+    var data:List<Restaurant> = getDataSet()
 }
