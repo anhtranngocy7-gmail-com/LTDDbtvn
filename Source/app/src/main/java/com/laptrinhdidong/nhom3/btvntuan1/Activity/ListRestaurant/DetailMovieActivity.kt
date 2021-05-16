c63f6f6f88d09103ec8b1298b45735afdb6b6a3c
NguyenQuocAn99 <nqan1999vvk@gmail.com>
2021-05-16 17:50:12 +0700

    test truyền bundle w6

--- Source/app/src/main/java/com/laptrinhdidong/nhom3/btvntuan1/Activity/ListRestaurant/DetailMovieActivity.kt
+++ Source/app/src/main/java/com/laptrinhdidong/nhom3/btvntuan1/Activity/ListRestaurant/DetailMovieActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
 import com.laptrinhdidong.nhom3.btvntuan1.Activity.MovieData.MovieViewModel
 import com.laptrinhdidong.nhom3.btvntuan1.R
 import com.laptrinhdidong.nhom3.btvntuan1.databinding.Nhom3datActivityDetailBinding
@@ -12,5 +13,8 @@ class DetailMovieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.nhom3dat_activity_detail)
+        binding.movieRate.text= "8.7"
+
+        Glide.with(this).load("link ảnh").centerCrop().placeholder(R.drawable.loading_icon).into(binding.moviePoster)
     }
 }
